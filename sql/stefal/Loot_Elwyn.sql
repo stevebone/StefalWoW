b74db6ef394b03a383722f4560711f50b0ecae6f
stevebone <zavoianu.stefan@gmail.com>
2024-06-30 01:07:14 +0200

    First version of some sql stuff
    - some cleanup of old stuff
    - 1st version of midsummer event spawns / templates for Eastern Kingdoms
    - add some of the old quests that were removed from Northshire.

---
+++ sql/stefal/Loot_Elwyn.sql
@@ -0,0 +1,4 @@
+# Update Loot Templates
+# Goblin Assassin - Northshire
+UPDATE `creature_template_difficulty` SET `LootID` = '50039', `PickPocketLootID` = '50039' WHERE (`Entry` = '50039') and (`DifficultyID` = '0');
+UPDATE `creature_template_difficulty` SET `LootID` = '50039', `PickPocketLootID` = '50039' WHERE (`Entry` = '50039') and (`DifficultyID` = '1');
