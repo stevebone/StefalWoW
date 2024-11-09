3f395183050eb8e8f239c9f02fdcd42bc41421c9
stevebone <zavoianu.stefan@gmail.com>
2024-11-09 19:17:13 +0100

    Misc Loot fixes
    - bag of shiny things missing loot
    - thunder falls fishing quest loot fix

---
+++ sql/stefal/loot_fixes.sql
@@ -0,0 +1,39 @@
+# Fishing
+INSERT INTO tc_world.fishing_loot_template VALUES (61, 1, 11000, 100, 0, 1, 1, 1,1,'Fishing ThunderFalls - Elwynn');
+INSERT INTO tc_world.reference_loot_template VALUES (11000, 0, 58899, 100, 1, 1, 1, 1,1,'Violet Perch');
+
+# Items
+# Bag of shiny things
+INSERT INTO tc_world.item_loot_template VALUES
+(67414, 0, 6532, 40, 0, 1, 1, 1, 3, 'Bag of shiny things'),
+(67414, 0, 8827, 15, 0, 1, 1, 1, 2, 'Bag of shiny things'),
+(67414, 0, 3820, 15, 0, 1, 1, 2, 5, 'Bag of shiny things'),
+(67414, 0, 2459, 15, 0, 1, 1, 1, 2, 'Bag of shiny things'),
+(67414, 0, 6372, 15, 0, 1, 1, 1, 2, 'Bag of shiny things'),
+(67414, 0, 67408, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67406, 5, 0, 1, 1, 1, 3, 'Bag of shiny things'),
+(67414, 0, 67397, 5, 0, 1, 1, 1, 3, 'Bag of shiny things'),
+(67414, 0, 67398, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67405, 5, 0, 1, 1, 1, 3, 'Bag of shiny things'),
+(67414, 0, 67411, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67409, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67404, 5, 0, 1, 1, 1, 3, 'Bag of shiny things'),
+(67414, 0, 67402, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67412, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67403, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67400, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67391, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67385, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67388, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67399, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67401, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67407, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67386, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 67410, 5, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 929, 5, 0, 1, 1, 1, 2, 'Bag of shiny things'),
+(67414, 0, 34834, 2, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 34109, 2, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 44983, 2, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 33820, 2, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 45991, 2, 0, 1, 1, 1, 1, 'Bag of shiny things'),
+(67414, 0, 45992, 2, 0, 1, 1, 1, 1, 'Bag of shiny things');
