b3f3d9b8e2f888e64f12697a852a0cd26b296665
stevebone <zavoianu.stefan@gmail.com>
2025-07-08 02:18:54 +0200

    Create 2025_07_08_00_world.sql
    Added graveyards for Gilneas zone as well as the upcoming Thalassian Highlands (from Turtle)

---
+++ sql/custom/world/2025_07_08_00_world.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES ('1721', '5179', '0', 'Gilneas, Stillward Church');
+INSERT IGNORE INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES ('1722', '5179', '0', 'Gilneas, Ravenwood Keep');
+INSERT IGNORE INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES ('1723', '5179', '0', 'Gilneas, Hollow Web Cemetery');
+INSERT IGNORE INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES ('1724', '2040', '0', 'Thalassian Highlands, Alah\'Thalas');
+INSERT IGNORE INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES ('1725', '5225', '0', 'Thalassian Highlands, Brinthillien');
