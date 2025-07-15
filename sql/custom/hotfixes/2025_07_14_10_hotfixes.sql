9074b52c8123ef9ccfc55f4e128843c5d309f045
stevebone <zavoianu.stefan@gmail.com>
2025-07-15 16:16:00 +0200

    Old quests reactivation
    - Reactivated deprecated quests in Shadowglen (webwood venom, egg and summons quests)
    - Reactivated deprecated quests in Shadowglen (balance of nature, and Nature's Due)
    - Reactivated Wildkin chain quests in Darnassus

---
+++ sql/custom/hotfixes/2025_07_14_10_hotfixes.sql
@@ -0,0 +1,4 @@
+-- Item: 5166 Webwood Venom Sac
+
+DELETE FROM `item` WHERE `ID` = 5166;
+INSERT IGNORE INTO `item` (`ID`, `ClassID`, `SubclassID`, `Material`, `InventoryType`, `SheatheType`, `SoundOverrideSubclassID`, `IconFileDataID`, `ItemGroupSoundsID`, `ContentTuningID`, `ModifiedCraftingReagentItemID`, `CraftingQualityID`, `VerifiedBuild`) VALUES ('5166', '12', '0', '0', '0', '0', '-1', '0', '0', '0', '0', '0', '0');
