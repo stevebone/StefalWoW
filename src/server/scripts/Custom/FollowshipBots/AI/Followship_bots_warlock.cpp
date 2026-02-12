a79a4275bedb1d733bb1c7a9cd2d317f9e5048aa
stevebone <zavoianu.stefan@gmail.com>
2026-02-12 14:46:04 +0100

    FSB warlock spells
    + added more spells for lock

--- src/server/scripts/Custom/FollowshipBots/AI/Followship_bots_warlock.cpp
+++ src/server/scripts/Custom/FollowshipBots/AI/Followship_bots_warlock.cpp
@@ -12,48 +12,55 @@ std::vector<FSBSpellDefinition> WarlockSpellsTable =
     //ANY
     { SPELL_WARLOCK_GIFT_NAARU,             FSBSpellType::Heal,     0.f,        50.f,       100.f,        40.f,          false,      120000,         FSB_RoleMask::FSB_ROLEMASK_ANY },
     { SPELL_WARLOCK_UNENDING_RESOLVE,       FSBSpellType::Damage,   0.f,        0.f,        50.f,         2.f,           true,       180000,         FSB_RoleMask::FSB_ROLEMASK_ANY },
-    { SPELL_WARLOCK_SHADOW_BOLT,            FSBSpellType::Damage,   0.f,        0.f,        90.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_ANY },
+    { SPELL_WARLOCK_SHADOW_BOLT,            FSBSpellType::Damage,   0.f,        0.f,        80.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_ANY },
 
-    { SPELL_WARLOCK_FEAR,                   FSBSpellType::Damage,   0.f,        0.f,        80.f,         20.f,          false,      8000,           FSB_RoleMask::FSB_ROLEMASK_ANY },
-    { SPELL_WARLOCK_DRAIN_LIFE,             FSBSpellType::Damage,   0.f,        0.f,        80.f,         20.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_ANY },
+    { SPELL_WARLOCK_FEAR,                   FSBSpellType::Damage,   0.f,        0.f,        70.f,         20.f,          false,      8000,           FSB_RoleMask::FSB_ROLEMASK_ANY },
+    { SPELL_WARLOCK_DRAIN_LIFE,             FSBSpellType::Damage,   0.f,        0.f,        40.f,         20.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_ANY },
     
     // AFFLICTION
     { SPELL_WARLOCK_CURSE_TONGUES,          FSBSpellType::Damage,   0.f,        0.f,        100.f,        40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
 
     { SPELL_WARLOCK_HAUNT,                  FSBSpellType::Damage,   0.2f,       0.f,        90.f,         40.f,          false,      15000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
     
-    { SPELL_WARLOCK_CORRUPTION,             FSBSpellType::Damage,   0.f,        0.f,        80.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
-    { SPELL_WARLOCK_AGONY,                  FSBSpellType::Damage,   0.f,        0.f,        80.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
-    { SPELL_WARLOCK_CURSE_WEAKNESS,         FSBSpellType::Damage,   0.f,        0.f,        80.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
+    { SPELL_WARLOCK_CORRUPTION,             FSBSpellType::Damage,   0.f,        0.f,        90.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
+    { SPELL_WARLOCK_AGONY,                  FSBSpellType::Damage,   0.f,        0.f,        90.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
+    { SPELL_WARLOCK_CURSE_WEAKNESS,         FSBSpellType::Damage,   0.f,        0.f,        90.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
     
     { SPELL_WARLOCK_SEED_CORRUPTION,        FSBSpellType::Damage,   0.f,        0.f,        70.f,         30.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
     { SPELL_WARLOCK_UNSTABLE_AFFLICTION,    FSBSpellType::Damage,   0.f,        0.f,        70.f,         30.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_AFFLICTION },
 
     // DEMONOLOGY
-    { SPELL_WARLOCK_WILD_IMPS,              FSBSpellType::Damage,   0.5f,       0.f,        100.f,        40.f,          false,      1800000,        FSB_RoleMask::FSB_ROLEMASK_RANGED_DEMONOLOGY },
     { SPELL_WARLOCK_DEMONIC_TYRANT,         FSBSpellType::Damage,   0.f,        0.f,        100.f,        40.f,          false,      60000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DEMONOLOGY },
     { SPELL_WARLOCK_CALL_DREADSTALKERS,     FSBSpellType::Damage,   0.05f,      0.f,        100.f,        40.f,          false,      20000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DEMONOLOGY },
     { SPELL_WARLOCK_DEMONBOLT,              FSBSpellType::Damage,   0.f,        0.f,        90.f,         40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_DEMONOLOGY },
+    { SPELL_WARLOCK_WILD_IMPS,              FSBSpellType::Damage,   0.5f,       0.f,        20.f,         40.f,          false,      1800000,        FSB_RoleMask::FSB_ROLEMASK_RANGED_DEMONOLOGY },
 
     // DESTRO
     { SPELL_WARLOCK_SUMMON_INFERNAL,        FSBSpellType::Damage,   0.3f,       0.f,        100.f,        40.f,          false,      180000,         FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
-    { SPELL_WARLOCK_CATACLYSM,              FSBSpellType::Damage,   0.3f,       0.f,        100.f,        40.f,          false,      30000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
-    { SPELL_WARLOCK_IMMOLATE,               FSBSpellType::Damage,   0.02f,      0.f,        100.f,        30.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
-    { SPELL_WARLOCK_INCINERATE,             FSBSpellType::Damage,   0.04f,      0.f,        100.f,        40.f,          false,      12000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_CHAOS_BOLT,             FSBSpellType::Damage,   0.04f,      0.f,         90.f,        40.f,          false,      10000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_SOUL_FIRE,              FSBSpellType::Damage,   0.04f,      0.f,         90.f,        40.f,          false,      45000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_IMMOLATE,               FSBSpellType::Damage,   0.02f,      0.f,         80.f,        30.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_SHADOWBURN,             FSBSpellType::Damage,   0.f,        0.f,         80.f,        40.f,          false,      1000,           FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_CONFLAGRATE,            FSBSpellType::Damage,   0.02f,      0.f,         70.f,        40.f,          false,      13000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_INCINERATE,             FSBSpellType::Damage,   0.04f,      0.f,         70.f,        40.f,          false,      12000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+    { SPELL_WARLOCK_CATACLYSM,              FSBSpellType::Damage,   0.3f,       0.f,         30.f,        40.f,          false,      30000,          FSB_RoleMask::FSB_ROLEMASK_RANGED_DESTRUCTION },
+
+
+
 
 };
 
 namespace FSBWarlock
 {
-    void HandleOnSpellCast(Creature* /*bot*/, uint32 spellId)
+    void HandleOnSpellCast(Creature* bot, uint32 spellId)
     {
-        //Unit* target = bot->GetVictim();
+        Unit* target = bot->GetVictim();
 
         switch (spellId)
         {
-        case SPELL_WARLOCK_CALL_DREADSTALKERS:
-            //if(target)
-            //    bot->CastSpell(target, SPELL_WARLOCK_CALL_DREADSTALKERS);
+        case SPELL_WARLOCK_CHAOS_BOLT:
+            if(target)
+                bot->CastSpell(target, SPELL_WARLOCK_CHAOS_BOLT_PERIODIC);
             break;
 
         default:
@@ -91,16 +98,19 @@ namespace FSBWarlock
             }
         }
 
-        else if(!bot->HasAura(spellId))
+        else 
         {
-            SpellCastResult result = bot->CastSpell(bot, spellId, false);
-            //bot->CastSpell(bot, visualId, false);
-            if (result == SPELL_CAST_OK)
+            if (!bot->HasAura(spellId))
             {
-                check = true;
-                TC_LOG_DEBUG("scripts.ai.fsb", "FSB: Warlock self SS buff pass");
+                SpellCastResult result = bot->CastSpell(bot, spellId, false);
+                //bot->CastSpell(bot, visualId, false);
+                if (result == SPELL_CAST_OK)
+                {
+                    check = true;
+                    TC_LOG_DEBUG("scripts.ai.fsb", "FSB: Warlock self SS buff pass");
+                }
+                else TC_LOG_DEBUG("scripts.ai.fsb", "FSB: Warlock self SS buff failed for bot: {} with result: {}", bot->GetName(), result);
             }
-            else TC_LOG_DEBUG("scripts.ai.fsb", "FSB: Warlock self SS buff failed for bot: {} with result: {}", bot->GetName(), result);
         }
 
         if (check)
