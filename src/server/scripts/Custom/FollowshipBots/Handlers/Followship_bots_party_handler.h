909d08ebe3a9ff43b22632bc875b9ca178205bd2
stevebone <zavoianu.stefan@gmail.com>
2026-03-07 19:12:22 +0100

    Update Followship_bots_party_handler.h
    Declarations for the new methods

--- src/server/scripts/Custom/FollowshipBots/Handlers/Followship_bots_party_handler.h
+++ src/server/scripts/Custom/FollowshipBots/Handlers/Followship_bots_party_handler.h
@@ -7,24 +7,24 @@ class Player;
 
 namespace FSBParty
 {
-    // Converts FSB_Class to WoW Classes enum value for party frame display
-    uint8 FSBClassToWoWClass(FSB_Class botClass);
-
-    // Converts FSB_Race to WoW Races enum value for faction group lookup
-    uint8 FSBRaceToWoWRace(FSB_Race botRace);
+    void PeriodicPartyNeededCheck(Creature* bot);
 
     // Sends a fake SMSG_PARTY_UPDATE to the player that includes their bots
     // If the player is in a real group, bots are appended to the real group members
     // If not in a group, creates a standalone fake party with just the player + bots
     void SendFakePartyUpdate(Player* player);
+    void SendFakePartyUpdate(Player* player, std::vector<Creature*> const& activeBots);
 
     // Sends SMSG_PARTY_MEMBER_FULL_STATE for a single bot creature to the owner
     // This updates health, mana, position etc. on the party frame
     void SendBotMemberState(Player* player, Creature* bot);
 
     // Sends health/mana/position updates for all bots to the owner
-    void SendAllBotMemberStates(Player* player);
+    void SendAllBotMemberStates(Player* player, std::vector<Creature*> const& activeBots);
 
     // Sends an empty party update to clear the fake group (on dismiss/logout)
     void SendClearFakeParty(Player* player);
+
+    void OnMemberAdd(Group* group, ObjectGuid guid);
+    void OnMemberRemove(Group* group, ObjectGuid guid);
 }
