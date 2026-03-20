678d42a78934fe51d7086fa2257c17c63e10aa18
stevebone <zavoianu.stefan@gmail.com>
2026-03-20 11:09:05 +0100

    DB/Westfall: small fix for npc faction

---
+++ sql/custom/world/EasternKingdoms/Westfall/SentinelHill.sql
@@ -0,0 +1,6 @@
+-- Sentinel Hill changes and updates
+
+-- NPC: 843 Gina Macgregor
+
+-- Creature Templates
+UPDATE `creature_template` SET `faction` = '12' WHERE `entry` IN (853); -- incorrect faction
