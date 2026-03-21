db698348387168d5eac6c26e8f843d4d147a8452
Shauren <shauren.trinity@gmail.com>
2026-03-21 15:26:53 +0100

    DB: Fix base auth_database.sql

--- sql/base/auth_database.sql
+++ sql/base/auth_database.sql
@@ -469,7 +469,7 @@ DROP TABLE IF EXISTS `battlenet_account_transmog_outfits`;
 CREATE TABLE `battlenet_account_transmog_outfits` (
   `battlenetAccountId` int unsigned NOT NULL,
   `transmogOutfitId` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`battlenetAccountId`,`blobIndex`),
+  PRIMARY KEY (`battlenetAccountId`,`transmogOutfitId`),
   CONSTRAINT `fk_battlenet_account_transmog_outfits` FOREIGN KEY (`battlenetAccountId`) REFERENCES `battlenet_accounts` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
