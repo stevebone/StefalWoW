674fd09a0e1c134a09bf617612263a046c543716
KamiliaBlow <25077548+KamiliaBlow@users.noreply.github.com>
2026-03-22 20:59:19 +0900

    Revert "Merge branch 'transmog-outfit-system' of https://github.com/Olcadoom/RoleplayCore"
    This reverts commit f4ced88f626a3a3e75c459c67d004eebc3ffd690, reversing
    changes made to 61411b3985c7894921852b3bb29214ed8d146e30.

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -834,10 +834,6 @@ namespace WorldPackets
     namespace Transmogrification
     {
         class TransmogrifyItems;
-        class TransmogOutfitUpdateSlots;
-        class TransmogOutfitNew;
-        class TransmogOutfitUpdateInfo;
-        class ClearNewAppearance;
     }
 
     namespace Vehicle
@@ -1817,10 +1813,6 @@ class TC_GAME_API WorldSession
 
         // Transmogrification
         void HandleTransmogrifyItems(WorldPackets::Transmogrification::TransmogrifyItems& transmogrifyItems);
-        void HandleTransmogOutfitUpdateSlots(WorldPackets::Transmogrification::TransmogOutfitUpdateSlots& packet);
-        void HandleTransmogOutfitNew(WorldPackets::Transmogrification::TransmogOutfitNew& packet);
-        void HandleTransmogOutfitUpdateInfo(WorldPackets::Transmogrification::TransmogOutfitUpdateInfo& packet);
-        void HandleClearNewAppearance(WorldPackets::Transmogrification::ClearNewAppearance& packet);
 
         // Miscellaneous
         void HandleSpellClick(WorldPackets::Spells::SpellClick& spellClick);
