6bb21dcefaf0c1a071f249341c01de7faf2862ae
stevebone <zavoianu.stefan@gmail.com>
2026-03-23 12:02:22 +0100

    Core/Transmog: Reverted DoomCore Transmog before TC update.

--- src/server/game/Spells/Spell.h
+++ src/server/game/Spells/Spell.h
@@ -449,7 +449,6 @@ class TC_GAME_API Spell
         void EffectSendChatMessage();
         void EffectGrantBattlePetExperience();
         void EffectLearnTransmogIllusion();
-        void EffectEquipTransmogOutfit();
         void EffectModifyAuraStacks();
         void EffectModifyCooldown();
         void EffectModifyCooldowns();
