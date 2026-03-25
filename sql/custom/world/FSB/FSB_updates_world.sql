ffa390fdafd2ffa14770f029b0d3da0e82ad3cf8
stevebone <zavoianu.stefan@gmail.com>
2026-03-25 15:24:58 +0100

    DB/Stormwind re enabled some old quests + misc
    + updated the stormwind file to TDB base
    + re added some old quests in Stormwind
    + spawned Germaine trainer
    - removed the TC update from the custom folder
    + started a todo list for things i may want to do in the future

--- sql/custom/world/FSB/FSB_updates_world.sql
+++ sql/custom/world/FSB/FSB_updates_world.sql
@@ -3,12 +3,6 @@
 -- Elwynn
 DELETE FROM `creature` WHERE `guid` = 280677;
 
--- Trainers Stormwind
--- 914/ander-germaine
-UPDATE `creature_template_difficulty` SET `ContentTuningID` = '864' WHERE (`Entry` = '914') and (`DifficultyID` = '0');
-DELETE FROM `creature` WHERE `ID` = 914;
-INSERT INTO `creature` VALUES
-('900267', '914', '0', '1519', '1519', '0', '0', '0', '0', '-1', '0', '1', '-8795.63', '356.435', '107.27', '6.18479', '300', '0', '0', '100', '0', NULL, NULL, NULL, NULL, '', NULL, '0');
 
 -- 63331/laoxi
 DELETE FROM `creature` WHERE `ID` = 63331;
