8c12d75fb05751bb51dc408d93a81782642ba7f2
stevebone <zavoianu.stefan@gmail.com>
2026-03-27 02:13:04 +0100

    DB/Westfall and misc
    + added some cleanup queries for models to remove non existing models and add missing ones that exist on creature data
    + added The Defias Brotherhood quests back in
    + added spawns and scripts for defias messenger/traitor
    + added some missing scripts for existing mobs
    + added all the spawns for People's militia - still might need to add some more in Moonbrook once the new phase is applied - the place will feel kind of empty
    + many creature_text added plus difficulties tweaks and loot tables
    + added waypoints for Slark/traitor/messenger
    + added an extra phase for the defias related quests/npcs

--- sql/custom/hotfixes/Phases/Phases.sql
+++ sql/custom/hotfixes/Phases/Phases.sql
@@ -8,11 +8,13 @@ REPLACE INTO `hotfix_data` (`Id`, `UniqueId`, `TableHash`, `RecordId`, `Status`,
 (800002, 800002, 4042326115, 50002, 1, -1), -- Start zone 2
 (800003, 800003, 4042326115, 50003, 1, -1), -- Start zone 2
 (800004, 800004, 4042326115, 50004, 1, -1), -- Westfall 1
-(800005, 800005, 4042326115, 50004, 1, -1); -- Westfall 2
+(800005, 800005, 4042326115, 50005, 1, -1), -- Westfall 2
+(800006, 800006, 4042326115, 50006, 1, -1); -- Westfall 3
 
 REPLACE INTO `phase` VALUES
 (50001, 4, -1), -- Start zone 1
 (50002, 4, -1), -- Start zone 2
 (50003, 4, -1), -- Start zone 2
 (50004, 4, -1), -- Westfall 1
-(50005, 4, -1); -- Westfall 2
+(50005, 4, -1), -- Westfall 2
+(50006, 4, -1); -- Westfall 3
--- sql/custom/world/EasternKingdoms/Westfall/WestfallSentinelHill.sql
+++ sql/custom/world/EasternKingdoms/Westfall/WestfallSentinelHill.sql
@@ -3,6 +3,10 @@
 -- NPC: 234 Gryan Stoutmantle
 -- NPC: 821 Captain Danuvin
 -- NPC: 878 Scout Galiaan
+-- NPC: 42575 Hope Saldean
+-- NPC: 42635 Ripsnarl
+
+-- GO: 204005 Heavy Wooden Stocks
 
 -- Quest: 12 The People's militia
 -- Quest: 13 The People's militia
@@ -10,26 +14,48 @@
 -- Quest: 102 Patrolling Westfall
 -- Quest: 153 Red Leather Bandanas
 
+-- Quest: 65 The Defias Brotherhood
+-- Quest: 132 The Defias Brotherhood
+-- Quest: 135 The Defias Brotherhood
+-- Quest: 141 The Defias Brotherhood
+-- Quest: 142 The Defias Brotherhood
+-- Quest: 155 The Defias Brotherhood
+-- Quest: 166 The Defias Brotherhood
+
 
 -- Reactivating old/deprecated quests in Sentinel Hill
-DELETE FROM `disables` WHERE (`sourceType` = '1') and `entry` IN (12, 13, 14, 102, 153);
+DELETE FROM `disables` WHERE (`sourceType` = '1') and `entry` IN (12, 13, 14, 102, 153, 65, 132, 135, 141, 142, 155, 166);
 
 -- Quest Templates
-UPDATE `quest_template` SET `ContentTuningID` = '6', `Expansion` = 0 WHERE `ID` IN (12, 13, 14, 102, 153);
+UPDATE `quest_template` SET `ContentTuningID` = '6', `Expansion` = 0 WHERE `ID` IN (12, 13, 14, 102, 153, 65, 132, 135, 141, 142, 155, 166);
 
 -- Creature Quest Relations
-DELETE FROM `creature_queststarter` WHERE `quest` IN (12, 13, 14, 102, 153);
+DELETE FROM `creature_queststarter` WHERE `quest` IN (12, 13, 14, 102, 153, 65, 132, 135, 141, 142, 155, 166);
 INSERT INTO `creature_queststarter` (`id`, `quest`, `VerifiedBuild`) VALUES 
 ('821', '102', '0'),
 ('878', '153', '0'),
 ('234', '12', '0'),
 ('234', '13', '0'),
-('234', '14', '0');
+('234', '14', '0'),
+('234', '65', '0'),
+('266', '132', '0'),
+('234', '135', '0'),
+('332', '141', '0'),
+('234', '142', '0'),
+('467', '155', '0'),
+('234', '166', '0');
 
-DELETE FROM `creature_questender` WHERE `quest` IN (12, 13, 14, 102, 153);
+DELETE FROM `creature_questender` WHERE `quest` IN (12, 13, 14, 102, 153, 65, 132, 135, 141, 142, 155, 166);
 INSERT INTO `creature_questender` (`id`, `quest`, `VerifiedBuild`) VALUES 
 ('821', '102', '0'),
 ('878', '153', '0'),
 ('234', '12', '0'),
 ('234', '13', '0'),
-('234', '14', '0');
+('234', '14', '0'),
+('266', '65', '0'),
+('234', '132', '0'),
+('332', '135', '0'),
+('234', '141', '0'),
+('234', '142', '0'),
+('234', '155', '0'),
+('234', '166', '0');
