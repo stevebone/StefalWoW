56e2cf2b891bb5299932aa3cb742f935522fbe81
stevebone <zavoianu.stefan@gmail.com>
2026-05-11 01:41:21 +0200

    DB/Events: Winterveil data
    - vendors update from https://github.com/TrinityCore/TrinityCore/issues/31597
    - dungeon/raids objects from https://github.com/TrinityCore/TrinityCore/issues/31596

---
+++ sql/custom/world/Events/Winterveil/Winterveil_vendors_1.sql
@@ -0,0 +1,216 @@
+DELETE FROM `npc_vendor` WHERE (`entry` IN (13429, 23012, 13432, 13435, 13420, 23010, 13433) AND `item` IN (234595, 234596, 234597, 234598, 234407, 234405, 234403, 234398, 234593, 234401, 234406, 234404, 234402, 234399, 234594, 234400) AND `type`=1) OR (`entry` IN (96304, 23064) AND `item` IN (34413, 34262, 34261, 34319, 234595, 234596, 234597, 234598, 234407, 234405, 234403, 234398, 234593, 234401, 234406, 234404, 234402, 234399, 234594, 234400, 17201, 17200, 17202, 17196, 17194, 17307, 17304, 17303, 188680, 70923) AND `type`=1) OR (`entry`=96305 AND `item` IN (17402, 17403, 34410, 17408, 17407, 17406, 34412, 17405, 17404, 21215) AND `type`=1) OR (`entry`=96362 AND `item` IN (108635, 128670, 128669, 128668, 128667, 128666, 128665, 33226, 128664, 128663, 128662, 128661, 128660) AND `type`=1);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `ExtendedCost`, `type`, `PlayerConditionID`, `IgnoreFiltering`, `VerifiedBuild`) VALUES
+-- Nardstrum Copperpinch
+(13429, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(13429, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(13429, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(13429, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(13429, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(13429, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(13429, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(13429, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(13429, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(13429, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(13429, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(13429, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(13429, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(13429, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(13429, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(13429, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+-- Hotoppik Copperpinch
+(23012, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(23012, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(23012, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(23012, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(23012, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(23012, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(23012, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(23012, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(23012, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(23012, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(23012, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(23012, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(23012, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(23012, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(23012, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(23012, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+-- Seersa Copperpinch 
+(13432, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(13432, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(13432, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(13432, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(13432, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(13432, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(13432, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(13432, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(13432, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(13432, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(13432, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(13432, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(13432, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(13432, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(13432, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(13432, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+-- Khole Jinglepocket
+(13435, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(13435, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(13435, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(13435, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(13435, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(13435, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(13435, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(13435, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(13435, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(13435, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(13435, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(13435, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(13435, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(13435, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(13435, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(13435, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+-- Penney Copperpinch
+(13420, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(13420, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(13420, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(13420, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(13420, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(13420, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(13420, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(13420, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(13420, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(13420, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(13420, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(13420, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(13420, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(13420, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(13420, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(13420, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+-- Izzy Hollyfizzle
+(96362, 13, 108635, 0, 5926, 1, 0, 0, 64978), -- Crashin' Thrashin' Killdozer Controller
+(96362, 12, 128670, 0, 5926, 1, 0, 0, 64978), -- Savage Gift
+(96362, 11, 128669, 0, 5926, 1, 0, 0, 64978), -- Old Box of Decorations
+(96362, 10, 128668, 0, 5926, 1, 0, 0, 64978), -- Festive Outfits
+(96362, 9, 128667, 0, 5926, 1, 0, 0, 64978), -- Little Helpers
+(96362, 8, 128666, 0, 5926, 1, 0, 0, 64978), -- Imported Trees
+(96362, 7, 128665, 0, 5926, 1, 0, 0, 64978), -- Ball of Tangled Lights
+(96362, 6, 33226, 0, 5925, 1, 0, 0, 64978), -- Tricky Treat
+(96362, 5, 128664, 0, 5924, 1, 0, 0, 64978), -- Creepy Crawlers
+(96362, 4, 128663, 0, 5924, 1, 0, 0, 64978), -- Witch's Brew
+(96362, 3, 128662, 0, 5924, 1, 0, 0, 64978), -- Seer's Invitation
+(96362, 2, 128661, 0, 5924, 1, 0, 0, 64978), -- Hallow's Glow
+(96362, 1, 128660, 0, 5924, 1, 0, 0, 64978), -- Ghoulish Guises
+-- Tradurjo Jinglepocket
+(96304, 29, 34413, 0, 0, 1, 0, 0, 64978), -- Recipe: Hot Apple Cider
+(96304, 28, 34262, 0, 0, 1, 0, 0, 64978), -- Pattern: Winter Boots
+(96304, 27, 34319, 0, 0, 1, 0, 0, 64978), -- Pattern: Red Winter Clothes
+(96304, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(96304, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(96304, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(96304, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(96304, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(96304, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(96304, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(96304, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(96304, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(96304, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(96304, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(96304, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(96304, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(96304, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(96304, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(96304, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+(96304, 10, 17201, 0, 0, 1, 0, 0, 64978), -- Recipe: Winter Veil Egg Nog
+(96304, 9, 17200, 0, 0, 1, 0, 0, 64978), -- Recipe: Gingerbread Cookie
+(96304, 8, 188680, 0, 0, 1, 0, 0, 64978), -- Winter Veil Chorus Book
+(96304, 7, 70923, 0, 0, 1, 0, 0, 64978), -- Gaudy Winter Veil Sweater
+(96304, 6, 17196, 0, 0, 1, 0, 0, 64978), -- Holiday Spirits
+(96304, 5, 17194, 0, 0, 1, 0, 0, 64978), -- Holiday Spices
+(96304, 4, 17307, 0, 0, 1, 0, 0, 64978), -- Purple Ribboned Wrapping Paper
+(96304, 3, 17304, 0, 0, 1, 0, 0, 64978), -- Green Ribboned Wrapping Paper
+(96304, 2, 17303, 0, 0, 1, 0, 0, 64978), -- Blue Ribboned Wrapping Paper
+(96304, 1, 17202, 0, 0, 1, 0, 0, 64978), -- Snowball
+-- Eebee Jinglepocket
+(23064, 29, 34413, 0, 0, 1, 0, 0, 64978), -- Recipe: Hot Apple Cider
+(23064, 28, 34262, 0, 0, 1, 0, 0, 64978), -- Pattern: Winter Boots
+(23064, 27, 34261, 0, 0, 1, 0, 0, 64978), -- Pattern: Green Winter Clothes
+(23064, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(23064, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(23064, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(23064, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(23064, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(23064, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(23064, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(23064, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(23064, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(23064, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(23064, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(23064, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(23064, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(23064, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(23064, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(23064, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+(23064, 8, 188680, 0, 0, 1, 0, 0, 64978), -- Winter Veil Chorus Book
+(23064, 7, 70923, 0, 0, 1, 0, 0, 64978), -- Gaudy Winter Veil Sweater
+-- Ashanem Jinglepocket
+(96305, 10, 17402, 0, 0, 1, 0, 0, 64978), -- Greatfather's Winter Ale
+(96305, 9, 17403, 0, 0, 1, 0, 0, 64978), -- Steamwheedle Fizzy Spirits
+(96305, 8, 34410, 0, 0, 1, 0, 0, 64978), -- Honeyed Holiday Ham
+(96305, 7, 17408, 0, 0, 1, 0, 0, 64978), -- Spicy Beefstick
+(96305, 6, 17407, 0, 0, 1, 0, 0, 64978), -- Graccu's Homemade Meat Pie
+(96305, 5, 17406, 0, 0, 1, 0, 0, 64978), -- Holiday Cheesewheel
+(96305, 4, 34412, 0, 0, 1, 0, 0, 64978), -- Sparkling Apple Cider
+(96305, 3, 17405, 0, 0, 1, 0, 0, 64978), -- Green Garden Tea
+(96305, 2, 17404, 0, 0, 1, 0, 0, 64978), -- Blended Bean Brew
+(96305, 1, 21215, 0, 0, 1, 0, 0, 64978), -- Graccu's Mince Meat Fruitcake
+-- Wolgren Jinglepocket
+(23010, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(23010, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(23010, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(23010, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(23010, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(23010, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(23010, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(23010, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(23010, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(23010, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(23010, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(23010, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(23010, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(23010, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(23010, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(23010, 11, 234400, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Coat
+-- Wulmort Jinglepocket
+(13433, 26, 234595, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Sweater
+(13433, 25, 234596, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Sweater
+(13433, 24, 234597, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shirt
+(13433, 23, 234598, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shirt
+(13433, 22, 234407, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Boots
+(13433, 21, 234405, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Shorts
+(13433, 20, 234403, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Pants
+(13433, 19, 234398, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Belt
+(13433, 18, 234593, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Vest
+(13433, 17, 234401, 0, 0, 1, 0, 0, 64978), -- Festive Green Holiday Coat
+(13433, 16, 234406, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Boots
+(13433, 15, 234404, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Shorts
+(13433, 14, 234402, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Pants
+(13433, 13, 234399, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Belt
+(13433, 12, 234594, 0, 0, 1, 0, 0, 64978), -- Festive Red Holiday Vest
+(13433, 11, 234400, 0, 0, 1, 0, 0, 64978); -- Festive Red Holiday Coat
+
+UPDATE `npc_vendor` SET `VerifiedBuild`=64978 WHERE `ExtendedCost`=0 AND `type`=1 AND ((`item` IN (17201, 17200, 188680, 70923, 17196, 17194, 17307, 17304, 17303, 17202) AND `entry` IN (13429, 13420, 13432, 13433, 13435, 23010, 23012)) OR (`item` IN (17402, 17403, 34410, 17408, 17407, 17406, 34412, 17405, 17404, 21215) AND `entry` IN (13418, 13431, 13434, 13436, 23009, 23011, 13430)));
+
+UPDATE `npc_vendor` SET `slot`=29, `VerifiedBuild`=64978 WHERE `item`=34413 AND `ExtendedCost`=0 AND `type`=1 AND `entry` IN (13429, 13420, 13432, 13433, 13435, 23010);
+UPDATE `npc_vendor` SET `slot`=28, `VerifiedBuild`=64978 WHERE `item`=34262 AND `ExtendedCost`=0 AND `type`=1 AND `entry` IN (13429, 13420, 13432, 13433, 13435, 23010);
+UPDATE `npc_vendor` SET `slot`=27, `VerifiedBuild`=64978 WHERE `item`=34261 AND `ExtendedCost`=0 AND `type`=1 AND `entry` IN (13429, 13420, 13432);
+UPDATE `npc_vendor` SET `slot`=27, `VerifiedBuild`=64978 WHERE `item`=34319 AND `ExtendedCost`=0 AND `type`=1 AND `entry` IN (13433, 13435, 23010);
+UPDATE `npc_vendor` SET `slot`=10, `VerifiedBuild`=64978 WHERE `item`=17201 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=9, `VerifiedBuild`=64978 WHERE `item`=17200 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=6, `VerifiedBuild`=64978 WHERE `item`=17196 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=5, `VerifiedBuild`=64978 WHERE `item`=17194 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=4, `VerifiedBuild`=64978 WHERE `item`=17307 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=3, `VerifiedBuild`=64978 WHERE `item`=17304 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=2, `VerifiedBuild`=64978 WHERE `item`=17303 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+UPDATE `npc_vendor` SET `slot`=1, `VerifiedBuild`=64978 WHERE `item`=17202 AND `ExtendedCost`=0 AND `type`=1 AND `entry`=23064;
+
+-- Candy Cane (17344) was replaced by Candy Cane (128768) available from Gently Shaken Gift
+DELETE FROM `npc_vendor` WHERE  `entry`=23064 AND `item`=17344 AND `ExtendedCost`=0 AND `type`=1;
