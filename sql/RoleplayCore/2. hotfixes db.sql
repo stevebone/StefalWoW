f9ae7c0abe4a97234a479af43276970ec6c7c75d
Kamilia Blow <windowstar24to7@gmail.com>
2026-05-26 14:08:11 +0900

    Update 2. hotfixes db.sql

--- sql/RoleplayCore/2. hotfixes db.sql
+++ sql/RoleplayCore/2. hotfixes db.sql
@@ -768,7 +768,9 @@ CREATE TABLE IF NOT EXISTS `perks_activity_x_interval` (
   PRIMARY KEY (`ID`,`VerifiedBuild`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---Ui Chromie Time Expansion info
+-- ----------------------------
+-- Table structure for ui_chromie_time_expansion_info
+-- ----------------------------
 DROP TABLE IF EXISTS `ui_chromie_time_expansion_info`;
 CREATE TABLE `ui_chromie_time_expansion_info`  (
   `ID` int UNSIGNED NOT NULL DEFAULT 0,
@@ -789,7 +791,9 @@ CREATE TABLE `ui_chromie_time_expansion_info`  (
   PRIMARY KEY (`ID`, `VerifiedBuild`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
--- Ui Chromie Time Expansion info Locale
+-- ----------------------------
+-- Table structure for ui_chromie_time_expansion_info_locale
+-- ----------------------------
 DROP TABLE IF EXISTS `ui_chromie_time_expansion_info_locale`;
 CREATE TABLE `ui_chromie_time_expansion_info_locale`  (
   `ID` int UNSIGNED NOT NULL DEFAULT 0,
@@ -800,4 +804,16 @@ CREATE TABLE `ui_chromie_time_expansion_info_locale`  (
   `HordeOverrideDesc_lang` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
   `VerifiedBuild` int NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`, `locale`, `VerifiedBuild`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+/*!50500 PARTITION BY LIST  COLUMNS(locale)
+(PARTITION deDE VALUES IN ('deDE') ENGINE = InnoDB,
+ PARTITION esES VALUES IN ('esES') ENGINE = InnoDB,
+ PARTITION esMX VALUES IN ('esMX') ENGINE = InnoDB,
+ PARTITION frFR VALUES IN ('frFR') ENGINE = InnoDB,
+ PARTITION itIT VALUES IN ('itIT') ENGINE = InnoDB,
+ PARTITION koKR VALUES IN ('koKR') ENGINE = InnoDB,
+ PARTITION ptBR VALUES IN ('ptBR') ENGINE = InnoDB,
+ PARTITION ruRU VALUES IN ('ruRU') ENGINE = InnoDB,
+ PARTITION zhCN VALUES IN ('zhCN') ENGINE = InnoDB,
+ PARTITION zhTW VALUES IN ('zhTW') ENGINE = InnoDB) */;
+ 
