d77a7f9c14ba9eac985363c541f56722afccbd2c
stevebone <zavoianu.stefan@gmail.com>
2026-05-27 21:06:52 +0200

    Core/Spells: fix wrong target check for spell reflect

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2520,7 +2520,7 @@ void Spell::AddUnitTarget(Unit* target, uint32 effectMask, bool checkIfValid /*=
         Unit* unitCaster = ASSERT_NOTNULL(m_caster->ToUnit());
 
         // Calculate reflected spell result on caster
-        targetInfo.ReflectResult = m_spellInfo->CheckTarget(target, unitCaster, implicit) == SPELL_CAST_OK
+        targetInfo.ReflectResult = m_spellInfo->CheckTarget(unitCaster, unitCaster, implicit) == SPELL_CAST_OK
             ? unitCaster->SpellHitResult(unitCaster, m_spellInfo,
                 false /*can't reflect twice*/,
                 false /*immunity will be checked after complete EffectMask is known*/)
