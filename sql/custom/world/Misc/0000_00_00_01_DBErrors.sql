86f9ae1184c97e87e1ddfe75d9c90e9629234778
stevebone <zavoianu.stefan@gmail.com>
2026-05-31 01:43:05 +0200

    DB/Errors: fixed a few gameobject loot template error messages

--- sql/custom/world/Misc/0000_00_00_01_DBErrors.sql
+++ sql/custom/world/Misc/0000_00_00_01_DBErrors.sql
@@ -1,2 +1,8 @@
 -- Delete Totem models for non existing spells
 DELETE FROM `spell_totem_model` WHERE `SpellID` IN (157153);
+
+-- Delete Incorrect GO Loot
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 1731; -- copper vein has a different loot id, keeping that one.
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 1732; -- tin vein has a different loot id, keeping that one.
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 1733; -- silver vein has a different loot id, keeping that one.
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 1734; -- gold vein has a different loot id, keeping that one.
