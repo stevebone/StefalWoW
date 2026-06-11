d69bc9c6d57384f03ee946ddafdf9f075d1690aa
stevebone <zavoianu.stefan@gmail.com>
2026-06-11 15:47:25 +0200

    DB/Deadmines: updated Molten Slag shield spell id
    the old spell did uber damage. there is a newer one which seems more ok

--- sql/custom/world/EasternKingdoms/Deadmines/Deadmines_Main.sql
+++ sql/custom/world/EasternKingdoms/Deadmines/Deadmines_Main.sql
@@ -253,7 +253,7 @@ SET @ENTRY := 49229;
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `action_param7`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`, `Difficulties`) VALUES
-(@ENTRY, 0, 0, 1, 11, 0, 100, 0, 0, 36, 0, 0, 0, 11, 91815, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On respawn on map 36 - Self: Cast spell  91815 on Self', ''),
+(@ENTRY, 0, 0, 1, 11, 0, 100, 0, 0, 36, 0, 0, 0, 11, 1217811, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On respawn on map 36 - Self: Cast spell  1217811 on Self', ''),
 (@ENTRY, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 0, 25, 100, 0, 0, 0, 0, 0, 0, 'On respawn on map 36 - Self: Attack Closest enemy creature in 50 yards', ''),
 (@ENTRY, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 86, 91830, 0, 1, 0, 0, 0, 0, 25, 100, 0, 0, 0, 0, 0, 0, 'On respawn on map 36 - Self: Cast spell  91830 at Closest enemy creature in 100 yards', '');
 
