943aed10e73a924afad66e7afcd2201a20d3b821
stevebone <zavoianu.stefan@gmail.com>
2026-06-14 16:06:21 +0200

    Scripts/Deadmines: removed old file for foereaper

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -65,7 +65,6 @@ void AddSC_boss_chromaggus();
 void AddSC_boss_nefarian();
 void AddSC_instance_blackwing_lair();
 void AddSC_instance_deadmines();             //Deadmines
-void AddSC_boss_foereaper5000();
 void AddSC_boss_admiral_ripsnarl();
 void AddSC_boss_captain_cookie();
 void AddSC_gilneas_chapter_1();              //Gilneas
@@ -269,7 +268,6 @@ void AddEasternKingdomsScripts()
     AddSC_boss_nefarian();
     AddSC_instance_blackwing_lair();
     AddSC_instance_deadmines();             //Deadmines
-    AddSC_boss_foereaper5000();
     AddSC_boss_admiral_ripsnarl();
     AddSC_boss_captain_cookie();
     AddSC_gilneas_chapter_1();              //Gilneas
