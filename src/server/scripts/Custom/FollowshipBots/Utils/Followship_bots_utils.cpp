a25d507e506a8f42bcec556827f918b505acc3b3
stevebone <zavoianu.stefan@gmail.com>
2026-07-16 18:33:08 +0200

    Scripts/FSB: replace worgen language for bots with common as it is not working/used in core

--- src/server/scripts/Custom/FollowshipBots/Utils/Followship_bots_utils.cpp
+++ src/server/scripts/Custom/FollowshipBots/Utils/Followship_bots_utils.cpp
@@ -244,7 +244,7 @@ namespace FSBUtils
         case FSB_Race::NightElf:            return LANG_DARNASSIAN;
         case FSB_Race::Gnome:               return LANG_GNOMISH;
         case FSB_Race::Draenei:             return LANG_DRAENEI;
-        case FSB_Race::Worgen:              return LANG_WORGEN;
+        case FSB_Race::Worgen:              return LANG_COMMON;
         case FSB_Race::Pandaren:            return LANG_PANDAREN_ALLIANCE;
         case FSB_Race::VoidElf:             return LANG_THALASSIAN;
         case FSB_Race::Orc:                 return LANG_ORCISH;
