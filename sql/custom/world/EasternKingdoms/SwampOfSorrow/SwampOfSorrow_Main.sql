ee1193aad476e23e928e35d5f027cf7453a5cf72
stevebone <zavoianu.stefan@gmail.com>
2026-07-22 15:19:09 +0200

    DB/Misc: added some fixes from Nokreb

---
+++ sql/custom/world/EasternKingdoms/SwampOfSorrow/SwampOfSorrow_Main.sql
@@ -0,0 +1,14 @@
+-- Fixes for Swamp Of Sorrow
+
+-- NPC: 986 Haromm
+-- NPC: 51983 Deadwind Widow
+
+-- Wrong spawns
+DELETE FROM creature WHERE id = 51983 AND guid = 293089;
+DELETE FROM creature WHERE id = 51983 AND guid = 293088;
+
+-- Difficulty fixes
+-- Missing Content Tuning
+UPDATE creature_template_difficulty SET ContentTuningID = 31 WHERE Entry = 986 AND DifficultyID = 0;
+
+
