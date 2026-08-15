64e36849efe88b773814ffacb9571cfba28c9ef1
stevebone <zavoianu.stefan@gmail.com>
2026-08-15 14:41:05 +0200

    DB/Westfall: improvements to the zone
    - cleanup some creature difficulty and add missing loot
    - fix incorrect flags for 44089
    - set a dead 44089 for quest 26717/the-yorgen-worgen
    - added skinning loot for zone 3

--- sql/custom/world/EasternKingdoms/Duskwood/Duskwood_Main.sql
+++ sql/custom/world/EasternKingdoms/Duskwood/Duskwood_Main.sql
@@ -1,8 +1,39 @@
 -- Duskwood Fixes
 
+-- NPC: 48 Skeletal Warrior
+-- NPC: 202 Rotting Horror
+-- NPC: 203 Skeletal Mage
+-- NPC: 217 Venom Web Spider
+-- NPC: 511 Insane Ghoul
+-- NPC: 533 Nightbane Shadow Weaver
+-- NPC: 898 Nightbane Worgen
+-- NPC: 930 Black Widow Hatchling
+-- NPC: 1270 Fetid Corpse
+-- NPC: 43704 Dire Wolf
+-- NPC: 43732 Corpseweed
+-- NPC: 44087 Nightbane Stalker
+-- NPC: 44089 Blackbelly Forager
+
+-- Quest: 26717 The Yorgen Worgen
+
+-- Creature Difficulty
+DELETE FROM `creature_template_difficulty` WHERE `DifficultyID` = 1 AND `Entry` IN (
+48, 202, 203, 217, 511, 533, 898, 930, 1270, 43704, 43732, 44087, 44089
+);
+
+UPDATE `creature_template_difficulty` SET `DamageModifier` = 0.2 WHERE `Entry` IN (48, 202, 203, 217, 511, 533, 898, 930, 1270, 43704, 43732, 44087, 44089);
+
+UPDATE `creature_template_difficulty` SET `SkinLootID` = 3 WHERE `Entry` = 43704;
+UPDATE `creature_template_difficulty` SET `LootID` = 44087 WHERE `Entry` = 44087;
+UPDATE `creature_template_difficulty` SET `LootID` = 44089, `SkinLootID` = 3 WHERE `Entry` = 44089;
+
+-- Remove incorrect template flags
+UPDATE `creature_template` SET `unit_flags` = 0 WHERE `entry` IN (44089);
+
 -- Wrong Creature Spawns
 DELETE FROM `creature` WHERE `guid` = 317602 AND `id` = 494;
 DELETE FROM `creature` WHERE `guid` = 317599 AND `id` = 228;
+DELETE FROM `creature` WHERE `guid` = 317779 AND `id` = 272;
 
 -- New Creature Spawns
 SET @CGUID := 900000;
@@ -18,7 +49,7 @@ SET MovementType = 1,
 WHERE id IN (
     889,212,628,44016,44020,210,604,948,43923,45614,
     43732,44089,44087,533,202,930,898,205,45517,217,
-    48,203,206,920,3
+    48,203,206,920,3,827,828
 );
 
 -- Creature Spawn Fixes
@@ -26,5 +57,13 @@ UPDATE creature
 SET MovementType = 1,
     wander_distance = 3
 WHERE id IN (
-    494,228,886
+    494,228,886,999
 );
+
+-- Specific quest related creature spawn changes
+-- 317260 dead Blackbelly Forager at Yorgen Farmstead
+UPDATE creature SET MovementType = 0, wander_distance = 0, `unit_flags` = 537133824, `unit_flags3` = 8192 WHERE `guid` IN (317260);
+-- Creature Addons
+DELETE FROM creature_addon WHERE guid IN (317260);
+INSERT INTO creature_addon (guid, StandState) VALUES (317260, 7);
+
--- sql/custom/world/Tables/skinning_loot_template.sql
+++ sql/custom/world/Tables/skinning_loot_template.sql
@@ -1,9 +1,15 @@
 -- Skinning loot fix
-DELETE FROM `skinning_loot_template` WHERE `Entry` IN (1,2);
+DELETE FROM `skinning_loot_template` WHERE `Entry` IN (1,2,3);
 INSERT INTO `skinning_loot_template` VALUES
-('1', '0', '2318', '70', '0', '1', '1', '1', '3', 'Starting Zones'),
-('1', '0', '2934', '75', '0', '1', '1', '1', '4', 'Starting Zones'),
+(1, 0, 2318, 70, 0, 1, 1, 1, 3, 'Starting Zones'),
+(1, 0, 2934, 75, 0, 1, 1, 1, 4, 'Starting Zones'),
 
-('2', '0', '783', '2', '0', '1', '1', '1', '6', 'Zones 2'), -- Light Hide
-('2', '0', '2318', '50', '0', '1', '1', '1', '7', 'Zones 2'), -- Light Leather
-('2', '0', '2934', '50', '0', '1', '1', '1', '6', 'Zones 2'); -- Ruined Leather Scraps
+(2, 0, 783, 2, 0, 1, 1, 1, 6, 'Zones 2'), -- Light Hide
+(2, 0, 2318, 50, 0, 1, 1, 1, 7, 'Zones 2'), -- Light Leather
+(2, 0, 2934, 50, 0, 1, 1, 1, 6, 'Zones 2'), -- Ruined Leather Scraps
+
+(3, 0, 783, 4, 0, 1, 1, 1, 5, 'Zones 3'), -- Light Hide
+(3, 0, 2318, 60, 0, 1, 1, 1, 7, 'Zones 3'), -- Light Leather
+(3, 0, 2934, 5, 0, 1, 1, 1, 2, 'Zones 3'), -- Ruined Leather Scraps
+(3, 0, 2319, 35, 0, 1, 1, 1, 6, 'Zones 3'), -- Medium Leather
+(3, 0, 4232, 3, 0, 1, 1, 1, 5, 'Zones 3'); -- Medium Hide
