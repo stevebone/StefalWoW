2f4ae866974e50afd54ac75cdf2c650999eecc0e
stevebone <zavoianu.stefan@gmail.com>
2026-08-16 17:25:30 +0200

    DB/Duskwood: Add creature texts for quest Mistmantle's Revenge

---
+++ sql/custom/world/EasternKingdoms/Duskwood/Duskwood_CreatureText.sql
@@ -0,0 +1,16 @@
+-- Duskwood Creature Texts
+
+DELETE FROM `creature_text` WHERE `CreatureID` IN ();
+INSERT INTO `creature_text` VALUES
+(315, 0, 0, 'My ring... Who holds my family ring... Tilloa, is that you?', 12, 0, 100, 0, 0, 0, 0, 43963, 0, 'Stalvan Mistmantle - to Player'),
+(315, 1, 0, 'Tobias...', 12, 0, 100, 0, 0, 0, 0, 43964, 0, 'Stalvan Mistmantle to Player'),
+(315, 2, 0, 'It''s all true, brother. Every word. You doubted it?', 12, 0, 100, 11, 0, 0, 0, 43965, 0, 'Stalvan Mistmantle to Player'),
+(315, 3, 0, 'You know why!', 12, 0, 100, 25, 0, 0, 0, 43968, 0, 'Stalvan Mistmantle to Player'),
+(315, 4, 0, 'Surely you''ve felt anger. Anger so foul and vicious that it makes you want to tear someone to shreds...', 12, 0, 100, 1, 0, 0, 0, 43978, 0, 'Stalvan Mistmantle to Player'),
+(315, 5, 0, 'Aren''t you feeling it right now?', 12, 0, 100, 6, 0, 0, 0, 43979, 0, 'Stalvan Mistmantle to Player'),
+(315, 6, 0, 'You see, brother... we''re not so different...', 12, 0, 100, 6, 0, 0, 0, 43981, 0, 'Stalvan Mistmantle to Tobias Mistmantle'),
+(43453, 0, 0, 'Brother!', 12, 0, 100, 0, 0, 0, 0, 43966, 0, 'Tobias Mistmantle to Player'),
+(43453, 1, 0, 'Tell me it''s not true, brother. Tell me you didn''t die a murderer!', 12, 0, 100, 25, 0, 0, 0, 43967, 0, 'Tobias Mistmantle to Player'),
+(43453, 2, 0, 'But why?! How could you?', 12, 0, 100, 5, 0, 0, 0, 43973, 0, 'Tobias Mistmantle to Player'),
+(43797, 3, 0, 'No... NO! STOP IT!', 14, 0, 100, 15, 0, 0, 0, 43980, 0, 'Tobias Mistmantle to Player'),
+(43797, 4, 0, 'No...', 12, 0, 100, 0, 0, 0, 0, 43982, 0, 'Tobias Mistmantle to Stalvan Mistmantle');
