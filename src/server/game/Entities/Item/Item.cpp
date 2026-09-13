3b200abfe94ff1e761266f9a3de06c2924bd737a
stevebone <zavoianu.stefan@gmail.com>
2026-09-13 17:17:24 +0200

    Revert "Core/Player: fix gray items core bug with scaling"
    This reverts commit ed40091c559e9daeacd88817fc3fc58399491749.

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -2341,12 +2341,7 @@ uint32 Item::GetItemLevel(ItemTemplate const* itemTemplate, BonusData const& bon
         itemLevel += bonusData.ItemLevelBonus;
     }
     else
-    {
-        uint32 curveInput = bonusData.ItemLevelOffsetItemLevel;
-        if (!curveInput && bonusData.ItemLevelOffset < 0)
-            curveInput = itemTemplate->GetBaseItemLevel();
-        itemLevel = std::max(int32(MIN_ITEM_LEVEL), bonusData.ItemLevelOffset + int32(std::round(sDB2Manager.GetCurveValueAt(bonusData.ItemLevelOffsetCurveId, curveInput))));
-    }
+        itemLevel = bonusData.ItemLevelOffset + uint32(std::round(sDB2Manager.GetCurveValueAt(bonusData.ItemLevelOffsetCurveId, bonusData.ItemLevelOffsetItemLevel)));
 
     for (uint32 i = 0; i < MAX_ITEM_PROTO_SOCKETS; ++i)
         itemLevel += bonusData.GemItemLevelBonus[i];
